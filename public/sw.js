29dac38a55154b25ee11d4ca91ac57111255bfa7
idihemedidieumerci1646-cyber <idihemedidieumerci1646@gmail.com>
2026-07-27 12:34:46 +0100

    mis en jour

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,64 @@
-const CACHE_NAME = "biso-commerce-v5";
+const CACHE_NAME = "biso-commerce-v7";
+
+
+const APP_SHELL = [
+  "/",
+  "/dashboard",
+  "/products",
+  "/products/edit",
+  "/sales",
+  "/manifest.json",
+  "/icon.png"
+];
+
 
 
 self.addEventListener("install", (event)=>{
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then((cache)=>{
+
+      return cache.addAll(APP_SHELL);
+
+    })
+
+  );
+
+
   self.skipWaiting();
+
 });
+
+
+
 
 
 self.addEventListener("activate",(event)=>{
 
   event.waitUntil(
-    self.clients.claim()
+
+    caches.keys()
+    .then((keys)=>{
+
+      return Promise.all(
+
+        keys
+        .filter((key)=> key !== CACHE_NAME)
+        .map((key)=> caches.delete(key))
+
+      );
+
+    })
+    .then(()=>self.clients.claim())
+
   );
 
 });
+
+
+
 
 
 self.addEventListener("fetch",(event)=>{
@@ -20,42 +66,8 @@ self.addEventListener("fetch",(event)=>{
 
   event.respondWith(
 
-    fetch(event.request)
-    .then((response)=>{
-
-
-      if(
-        response &&
-        response.status === 200 &&
-        response.type === "basic"
-      ){
-
-        const clone = response.clone();
-
-
-        caches.open(CACHE_NAME)
-        .then((cache)=>{
-
-          cache.put(
-            event.request,
-            clone
-          );
-
-        });
-
-      }
-
-
-      return response;
-
-
-    })
-    .catch(async()=>{
-
-
-      const cached = await caches.match(
-        event.request
-      );
+    caches.match(event.request)
+    .then((cached)=>{
 
 
       if(cached){
@@ -65,15 +77,75 @@ self.addEventListener("fetch",(event)=>{
       }
 
 
-      return new Response(
-        "Biso-Commerce hors connexion",
-        {
-          status:503,
-          headers:{
-            "Content-Type":"text/plain"
-          }
+
+      return fetch(event.request)
+
+      .then((response)=>{
+
+
+        if(
+          response &&
+          response.status === 200 &&
+          response.type === "basic"
+        ){
+
+
+          const clone = response.clone();
+
+
+          caches.open(CACHE_NAME)
+          .then((cache)=>{
+
+            cache.put(
+              event.request,
+              clone
+            );
+
+          });
+
+
         }
-      );
+
+
+
+        return response;
+
+
+      })
+
+
+
+      .catch(()=>{
+
+
+        return caches.match("/")
+        .then((home)=>{
+
+
+          if(home){
+
+            return home;
+
+          }
+
+
+          return new Response(
+            "Biso-Commerce hors connexion",
+            {
+              status:503,
+              headers:{
+                "Content-Type":"text/plain"
+              }
+            }
+          );
+
+
+        });
+
+
+
+      });
+
 
 
     })
